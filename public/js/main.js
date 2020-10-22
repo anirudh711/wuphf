ae9589603cf802977dc9c86f20c3ba9c58a9afff
Anirudh Madhavan <anirudhragavender@gmail.com>
2020-10-22 20:19:41 +0530

    user rooms initialized

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,21 @@
-const chatForm =document.getElementById('chat-form')
-const chatMessages=document.querySelector('.chat-messages')
+const chatForm =document.getElementById('chat-form');
+const chatMessages=document.querySelector('.chat-messages');
+//get username and room from url
+const {username, room}=Qs.parse(location.search,{
+    ignoreQueryPrefix:true //to ignore & and other symbols
+});
+console.log(username, room)
 const socket=io();
 
+//join chat room
+socket.emit('joinRoom',{username,room})
 //message from the server
 socket.on('message',message=>{
     console.log(message);
     outputMessage(message)
     //scroll down to focus 
     chatMessages.scrollTop=chatMessages.scrollHeight
-    //clearing input
-    e.target.elements.msg.value='';
-    e.target.elements.msg.focus();
-})
+})  
 
 chatForm.addEventListener('submit',e=>{
     e.preventDefault();
@@ -19,15 +23,19 @@ chatForm.addEventListener('submit',e=>{
     const msg=e.target.elements.msg.value;
     //emitting message to server
     socket.emit('chatMessage',msg)
-})
+     //clearing input
+     e.target.elements.msg.value='';
+     e.target.elements.msg.focus();
+ })
+
 
 //output message to dom
 function outputMessage(message){
     const div=document.createElement('div');
     div.classList.add('message');
-    div.innerHTML=`<p class="meta">Brad <span>9.12pm</span></p>
+    div.innerHTML=`<p class="meta">${message.username} <span>${message.time}</span></p>
     <p class="text">
-    ${message}
+    ${message.text}
     </p>
     `
     document.querySelector('.chat-messages').appendChild(div);
--- server.js
+++ server.js
@@ -1,31 +1,43 @@
-const express =require('express');
-const http =require('http');
-const path=require('path');
-const socketio =require('socket.io');
-const app =express()
-const server=http.createServer(app)
-const io=socketio(server)
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 //@static folder setup
-app.use(express.static(path.join(__dirname,'public')))
-//Run when client is connected 
-io.on('connection',socket=>{
-    console.log('New WS connection')
+app.use(express.static(path.join(__dirname, "public")));
+//@utils
+const formatMessage = require("./utils/messages.js");
+const {getCurrentUser,userJoin} =require("./utils/users")
+
+const botName = "Wuphf Bot";
+//Run when client is connected
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    const user=userJoin(socket.id,username,room)
+    socket.join(user.room)
     //welcoming current user
-    socket.emit('message','Welcome to chatCord');
+    socket.emit("message", formatMessage(botName, "Welcome to chatCord"));
 
     //broadcast when a user connects(emits to everyone except the user who is connecting)
-    socket.broadcast.emit('message','A user has join the chat');
-    //runs when client disconnects
-    socket.on('disconnect',()=>{
-        //let everyone know that the user has left the chat
-        io.emit('message','A user has left the chat')
-    });
-    //listen for chat message
-    socket.on('chatMessage',msg=>{
-        console.log(msg)
-        io.emit('message',msg)
-    })
-})
+    socket.broadcast.to(user.room).emit(
+      "message",
+      formatMessage(botName, `${user.username} has join the chat`)
+    );
+  });
 
-const PORT=3000 || process.env.PORT;
-server.listen(PORT, ()=>console.log(`Server running on port ${PORT}`))
+  //listen for chat message
+  socket.on("chatMessage", (msg) => {
+    console.log(msg);
+    io.emit("message", formatMessage("USER", msg));
+  });
+  //runs when client disconnects
+  socket.on("disconnect", () => {
+    //let everyone know that the user has left the chat
+    io.emit("message", formatMessage(botName, "A user has left the chat"));
+  });
+});
+
+const PORT = 3000 || process.env.PORT;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
